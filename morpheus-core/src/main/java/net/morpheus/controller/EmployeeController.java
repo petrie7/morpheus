811af1c9cdce197f4a3f3062f9f722cbf577e683
petrie7 <pt211@kent.ac.uk>
2016-04-06 17:32:06 +0100

    Pete -Manager can update skills of developers

--- morpheus-core/src/main/java/net/morpheus/controller/EmployeeController.java
+++ morpheus-core/src/main/java/net/morpheus/controller/EmployeeController.java
@@ -1,14 +1,15 @@
 package net.morpheus.controller;
 
 import net.morpheus.domain.Employee;
+import net.morpheus.domain.Role;
 import net.morpheus.persistence.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class EmployeeController {
@@ -16,15 +17,27 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping("/employee")
+    @RequestMapping(value = "/employee", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public Employee read(Principal principal) {
         return employeeRepository.findByName(principal.getName());
     }
 
-    @RequestMapping("/employee/all")
+    @RequestMapping(value = "/employee/all", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public List<Employee> readAll() {
         return employeeRepository.getAll();
+    }
+
+    @RequestMapping(value = "/employee", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public void update(@RequestBody Map<String, Object> employeeMap) {
+        employeeRepository.update(
+                new Employee(
+                        employeeMap.get("username").toString(),
+                        Role.valueOf(employeeMap.get("role").toString()),
+                        (Map<String, Integer>) employeeMap.get("skills")
+                )
+        );
     }
 }
