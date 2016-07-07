d356763485afabc8ae79a505c68f8e0d5d1fb08a
Jonathan Sharifi <jonathansharifi@gmail.com>
2016-07-07 22:13:25 +0100

    [#3] Jon: Adding some more static template fields

--- morpheus-core/src/main/java/net/morpheus/controller/TemplateController.java
+++ morpheus-core/src/main/java/net/morpheus/controller/TemplateController.java
@@ -77,12 +77,7 @@ public class TemplateController {
                                         .descriptionForJunior("Stores stuff in an Array List and calls it persistence")
                                         .descriptionForMid("Loves Oracle")
                                         .descriptionForSenior("Hates Oracle")
-                                        .build()
-                        )
-                ),
-                new Template(
-                        "Soft Skills",
-                        asList(
+                                        .build(),
                                 templateField()
                                         .fieldName("Estimating and Planning")
                                         .descriptionForJunior("Knows the difference in T-Shirt sizes")
@@ -94,6 +89,88 @@ public class TemplateController {
                                         .descriptionForJunior("Knows a design isn't the squares in BlueJ")
                                         .descriptionForMid("Has considered Clean Architecture by Uncle Bob")
                                         .descriptionForSenior("Has tried and discarded Clean Architecture by Uncle Bob")
+                                        .build()
+                        )
+                ),
+                new Template(
+                        "Soft Skills",
+                        asList(
+                                templateField()
+                                        .fieldName("Listening")
+                                        .descriptionForJunior("People talking without speaking")
+                                        .descriptionForMid("People hearing without listening")
+                                        .descriptionForSenior("People writing songs that voices never share")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Learning (How)")
+                                        .descriptionForJunior("Horton knows a How")
+                                        .descriptionForMid("Horton know a What")
+                                        .descriptionForSenior("Horton knows a Who")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Learning (What)")
+                                        .descriptionForJunior("What is this?")
+                                        .descriptionForMid("What is that?")
+                                        .descriptionForSenior("What is life?")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Communication")
+                                        .descriptionForJunior("What we've got hear is failure to communicate")
+                                        .descriptionForMid("Some men you just can't reach")
+                                        .descriptionForSenior("So you get what we had here last week")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Influencing Skills")
+                                        .descriptionForJunior("Give me an I")
+                                        .descriptionForMid("Give me an N")
+                                        .descriptionForSenior("Give me a Fluence")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Pair Programming")
+                                        .descriptionForJunior("Has spoken to a person before")
+                                        .descriptionForMid("Knows a person")
+                                        .descriptionForSenior("Likes people")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Mentoring")
+                                        .descriptionForJunior("A Level mentor to GCSE students")
+                                        .descriptionForMid("Student Resident in University Halls")
+                                        .descriptionForSenior("Yogi")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Knowledge Transfer")
+                                        .descriptionForJunior("Knows what knowledge is")
+                                        .descriptionForMid("Has some knowledge")
+                                        .descriptionForSenior("Can transfer said knowledge")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Feedback")
+                                        .descriptionForJunior("Can receive feedback")
+                                        .descriptionForMid("Can dispute feedback")
+                                        .descriptionForSenior("Ignores feedback entirely")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Recruitment")
+                                        .descriptionForJunior("Passes the unattended test")
+                                        .descriptionForMid("Passes the pair test")
+                                        .descriptionForSenior("Accepts the job")
+                                        .build()
+                        )
+                ),
+                new Template(
+                        "Business Skills",
+                        asList(
+                                templateField()
+                                        .fieldName("Functionality")
+                                        .descriptionForJunior("Likes Functionality")
+                                        .descriptionForMid("Loves Functionality")
+                                        .descriptionForSenior("Lives Functionality")
+                                        .build(),
+                                templateField()
+                                        .fieldName("Business Language / Terminology")
+                                        .descriptionForJunior("Knows what stuff is")
+                                        .descriptionForMid("Knows what stuff does")
+                                        .descriptionForSenior("Knows the meaning of life, the universe and everything")
                                         .build()
                         )
                 )
